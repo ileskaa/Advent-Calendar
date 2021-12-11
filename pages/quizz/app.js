39a8e5e20c3e399313c61154d806b2dc1616b805
ileskaa <80916126+ileskaa@users.noreply.github.com>
2021-12-11 11:53:09 +0100

    Add files via upload

--- pages/quizz/app.js
+++ pages/quizz/app.js
@@ -1,4 +1,7 @@
-window.addEventListener('resize', () => { //we pass an anonymous func to the eventListener
+function setHeight() {
   let vh100 = window.innerHeight
   document.documentElement.style.setProperty('--vh100', `${vh100}px`);
-})
+}
+setHeight()
+
+window.addEventListener('resize', () => setHeight())
