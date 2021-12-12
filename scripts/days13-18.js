d803e179e724398f2f2fbb26d9554827cdd56319
ileskaa <80916126+ileskaa@users.noreply.github.com>
2021-12-12 23:03:19 +0100

    Add files via upload

--- scripts/days13-18.js
+++ scripts/days13-18.js
@@ -1,4 +1,4 @@
-addPara(modals[13], textNode(`Ça y est on dépasse déjà la moitié ! Pour aujourd'hui je vous ai préparé un petit quiz sur le thème du cinéma. Si l'un de vous arrive à obtenir 10/10, je lui paie à boire dès que j'en aurai l'occasion. Par contre, je compte sur vous pour ne pas tricher...`))
+addPara(modals[13], textNode(`Ça y est on dépasse déjà la moitié ! Pour aujourd'hui je vous ai préparé un petit quiz sur le thème du cinéma. Si l'un de vous arrive à obtenir 10/10, qu'il prenne une capture d'écran et je lui paierai à boire dès que j'en aurai l'occasion. Par contre, je compte sur vous pour ne pas tricher...`))
 
 addPara(modals[13], textNode("Pour lancer le quizz, il suffit de cliquer ci-dessous."))
 
