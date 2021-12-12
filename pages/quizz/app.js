8b3eb7986d128b235f5717324d8b9d81b62d5316
ileskaa <80916126+ileskaa@users.noreply.github.com>
2021-12-12 22:28:11 +0100

    Delete pages/quizz directory

--- pages/quizz/app.js
+++
@@ -1,7 +0,0 @@
-function setHeight() {
-  let vh100 = window.innerHeight
-  document.documentElement.style.setProperty('--vh100', `${vh100}px`);
-}
-setHeight()
-
-window.addEventListener('resize', () => setHeight())
